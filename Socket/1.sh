87b1badd0e967e01543abb60a2bc991247aaf90c
xiaolong Huang <xiaolonghuang@sz-dl-159.autox.sz>
2022-07-28 11:21:22 +0800

    rm

--- Socket/1.sh
+++
@@ -1,5 +0,0 @@
-gnome-terminal -t "1234" -x bash -c "python3 client1.py"
-gnome-terminal -t "1234" -x bash -c "python3 client1.py"
-gnome-terminal -t "1234" -x bash -c "python3 client1.py"
-gnome-terminal -t "1234" -x bash -c "python3 client1.py"
-python3 client1.py
--- trans.sh
+++
@@ -1,5 +0,0 @@
-cd UI
-pyuic5 -o exit.py 1.ui
-cd ..
-python3 main.py
-
